23074c3cbb6d3dc63ce48c2eeca8f6442269becd
Tzeentche <ilyasugako87@gmail.com>
2019-08-14 23:56:03 +0300

    Initial commit

---
+++ src/edu/epam/StringArray/task1/CamelToSnake.java
@@ -0,0 +1,11 @@
+package edu.epam.StringArray.task1;
+
+//      Дан массив названий переменных в camelCase. Преобразовать названия в snake_case.
+
+public class CamelToSnake {
+
+    public static void main(String[] args) {
+
+        
+    }
+}

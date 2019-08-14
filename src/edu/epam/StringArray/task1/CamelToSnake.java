b8333017ac400ad8dfaf36894e4db47c824a3afb
Tzeentche <ilyasugako87@gmail.com>
2019-08-15 00:00:33 +0300

    Started developing decision for task 3.1.1 "camelCase - to snake_case" (EPAM, 3.String and basics of text processing)

--- src/edu/epam/StringArray/task1/CamelToSnake.java
+++ src/edu/epam/StringArray/task1/CamelToSnake.java
@@ -2,10 +2,12 @@ package edu.epam.StringArray.task1;
 
 //      Дан массив названий переменных в camelCase. Преобразовать названия в snake_case.
 
+import java.util.*;
+
 public class CamelToSnake {
 
     public static void main(String[] args) {
 
-        
+
     }
 }

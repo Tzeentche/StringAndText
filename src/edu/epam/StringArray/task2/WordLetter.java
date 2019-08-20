a5ee68c562bc7cc3ee8ed8b08ea35ae99d6326ab
Tzeentche <ilyasugako87@gmail.com>
2019-08-20 23:22:06 +0300

    Started developing decision for task 3.1.2 "Change Word to Letter" (EPAM, 3.String and basics of text processing)

---
+++ src/edu/epam/StringArray/task2/WordLetter.java
@@ -0,0 +1,11 @@
+package edu.epam.StringArray.task2;
+
+//      Замените в строке все вхождения 'word' на 'letter'.
+
+public class WordLetter {
+
+    public static void main(String[] args) {
+
+
+    }
+}

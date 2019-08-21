a75c6c6c7360b3e1128a645f16f2c5a16141e80d
Tzeentche <ilyasugako87@gmail.com>
2019-08-21 23:53:34 +0300

    Started developing decision for task 3.1.5 "Delete some spaces" (EPAM, 3.String and basics of text processing)

---
+++ src/edu/epam/StringArray/task5/KillSpaces.java
@@ -0,0 +1,11 @@
+package edu.epam.StringArray.task5;
+
+//      Удалить в строке все лишние пробелы, то есть серии подряд идущих пробелов заменить на одиночные пробелы.
+//      Крайние пробелы в строке удалить.
+
+public class KillSpaces {
+
+    public static void main(String[] args) {
+        
+    }
+}

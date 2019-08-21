fb3887d665c46f04b01c0572c933330561c6091a
Tzeentche <ilyasugako87@gmail.com>
2019-08-21 23:56:32 +0300

    Started developing decision for task 3.2.1 "Find some spaces" (EPAM, 3.String and basics of text processing)

---
+++ src/edu/epam/StringBuilder/task1/FindSpaces.java
@@ -0,0 +1,9 @@
+package edu.epam.StringBuilder.task1;
+
+//      Дан текст (строка). Найдите наибольшее количество подряд идущих пробелов в нем.
+public class FindSpaces {
+
+    public static void main(String[] args) {
+        
+    }
+}

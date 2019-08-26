64f96daa3c086077a65ba8beda6c73a1889b8c84
Tzeentche <ilyasugako87@gmail.com>
2019-08-26 23:47:02 +0300

    Started developing decision for task 3.2.5 "Counting of 'A'" (EPAM, 3.String and basics of text processing)

---
+++ src/edu/epam/StringBuilder/task5/ACounts.java
@@ -0,0 +1,11 @@
+package edu.epam.StringBuilder.task5;
+
+//      Подсчитать, сколько раз среди символов заданной строки встречается буква “а”.
+
+public class ACounts {
+
+    public static void main(String[] args) {
+
+        
+    }
+}

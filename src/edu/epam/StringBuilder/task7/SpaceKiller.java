8a923b478beedebae5068532a9b0a44ee13741fb
Tzeentche <ilyasugako87@gmail.com>
2019-08-26 23:54:00 +0300

    Started developing decision for task 3.2.7 "Delete spaces anddoubles in string" (EPAM, 3.String and basics of text processing)

---
+++ src/edu/epam/StringBuilder/task7/SpaceKiller.java
@@ -0,0 +1,12 @@
+package edu.epam.StringBuilder.task7;
+
+//      Вводится строка. Требуется удалить из нее повторяющиеся символы и все пробелы. Например,
+//      если было введено "abc cde def", то должно быть выведено "abcdef".
+
+public class SpaceKiller {
+
+    public static void main(String[] args) {
+
+        
+    }
+}

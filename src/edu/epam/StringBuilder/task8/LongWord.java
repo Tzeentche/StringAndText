443931ad3484d88fdcd450b6e987f18d293432ea
Tzeentche <ilyasugako87@gmail.com>
2019-08-27 23:11:37 +0300

    Started developing decision for task 3.2.8 "Find longest word" (EPAM, 3.String and basics of text processing)

---
+++ src/edu/epam/StringBuilder/task8/LongWord.java
@@ -0,0 +1,11 @@
+package edu.epam.StringBuilder.task8;
+
+//      Вводится строка слов, разделенных пробелами. Найти самое длинное слово и вывести его на экран.
+//      Случай, когда самых длинных слов может быть несколько, не обрабатывать.
+
+public class LongWord {
+
+    public static void main(String[] args) {
+        
+    }
+}

f710a81de8d3caa5b40cbb774fb75060ac1f80b2
Tzeentche <ilyasugako87@gmail.com>
2019-09-09 22:57:32 +0300

    Added methods to decision for task 3.2.2 "Added 'b' after every'a' " (EPAM, 3.String and basics of text processing)

--- src/edu/epam/StringBuilder/task2/ChangeChars.java
+++ src/edu/epam/StringBuilder/task2/ChangeChars.java
@@ -2,10 +2,31 @@ package edu.epam.StringBuilder.task2;
 
 //      В строке вставить после каждого символа 'a' символ 'b'.
 
+import java.util.*;
+
 public class ChangeChars {
 
     public static void main(String[] args) {
 
+    }
 
+    String stringHolder() {
+
+        return "";
+    }
+
+    String charFinder() {
+
+        return "";
+    }
+
+    String charAdder() {
+
+        return "";
+    }
+
+    String output() {
+
+        return "";
     }
 }

90c76c3a5fc6a3d0965f77638da5f8fbe0b3015e
Tzeentche <ilyasugako87@gmail.com>
2019-09-09 22:59:28 +0300

    Added methods to decision for task 3.2.1 "Find max spaces chain" (EPAM, 3.String and basics of text processing)

--- src/edu/epam/StringBuilder/task1/FindSpaces.java
+++ src/edu/epam/StringBuilder/task1/FindSpaces.java
@@ -1,9 +1,32 @@
 package edu.epam.StringBuilder.task1;
 
 //      Дан текст (строка). Найдите наибольшее количество подряд идущих пробелов в нем.
+
+import java.util.*;
+
 public class FindSpaces {
 
     public static void main(String[] args) {
 
+    }
+
+    String textHolder() {
+
+        return "";
+    }
+
+    String spaceFinder() {
+
+        return "";
+    }
+
+    String spaceCounter() {
+
+        return "";
+    }
+
+    String output() {
+
+        return "";
     }
 }

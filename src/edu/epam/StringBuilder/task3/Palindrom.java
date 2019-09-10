cee45ec385ced16e5c12f7df6c6d27a2ee2b367d
Tzeentche <ilyasugako87@gmail.com>
2019-09-10 23:55:19 +0300

    Added methods to decision for task 3.2.3 "Palindrom" (EPAM, 3.String and basics of text processing)

--- src/edu/epam/StringBuilder/task3/Palindrom.java
+++ src/edu/epam/StringBuilder/task3/Palindrom.java
@@ -3,4 +3,28 @@ package edu.epam.StringBuilder.task3;
 //      Проверить, является ли заданное слово палиндромом.
 
 public class Palindrom {
+
+    public static void main(String[] args) {
+
+    }
+
+    String enteredString() {
+
+        return "";
+    }
+
+    String separator() {
+
+        return "";
+    }
+
+    String comparator() {
+
+        return "";
+    }
+
+    String output() {
+
+        return "";
+    }
 }

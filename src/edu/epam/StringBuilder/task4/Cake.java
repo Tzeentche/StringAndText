bacde647a803c8347aaf725bdabc5efc7f590dff
Tzeentche <ilyasugako87@gmail.com>
2019-09-10 23:56:49 +0300

    Added methods to decision for task 3.2.4 "Create word cake" (EPAM, 3.String and basics of text processing)

--- src/edu/epam/StringBuilder/task4/Cake.java
+++ src/edu/epam/StringBuilder/task4/Cake.java
@@ -3,4 +3,28 @@ package edu.epam.StringBuilder.task4;
 //С помощью функции копирования и операции конкатенации составить из частей слова “информатика” слово “торт”.
 
 public class Cake {
+
+    public static void main(String[] args) {
+        
+    }
+
+    String enteredString() {
+
+        return "";
+    }
+
+    String copyr() {
+
+        return "";
+    }
+
+    String concatenator() {
+
+        return "";
+    }
+
+    String output() {
+
+        return "";
+    }
 }

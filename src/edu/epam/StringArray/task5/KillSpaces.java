ba7a150e08a959bfd744e665e39538b8a2324af5
Tzeentche <ilyasugako87@gmail.com>
2020-01-16 22:51:37 +0300

    Added methods to decision for task 3.2.5 (EPAM, 3.String and basics of text processing)

--- src/edu/epam/StringArray/task5/KillSpaces.java
+++ src/edu/epam/StringArray/task5/KillSpaces.java
@@ -3,6 +3,8 @@ package edu.epam.StringArray.task5;
 //      Удалить в строке все лишние пробелы, то есть серии подряд идущих пробелов заменить на одиночные пробелы.
 //      Крайние пробелы в строке удалить.
 
+import java.util.*;
+
 public class KillSpaces {
 
     public static void main(String[] args) {
@@ -13,9 +15,26 @@ public class KillSpaces {
 
     private void newSentence() {
 
+        Scanner scn = new Scanner(System.in);
+        String sentense = "";
+
+        System.out.println("Please, enter your sentense: ");
+
+        try {
+
+            sentense = scn.nextLine();
+            scn.close();
+
+        } catch(InputMismatchException ex1) {
+
+            System.out.println("You done something wrong, and broke it all.");
+        }
+
+        cloneKiller(sentense);
     }
 
-    private void kloneKiller() {
+    private void cloneKiller(String usersExpression) {
+
 
     }
 

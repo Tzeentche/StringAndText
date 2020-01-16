20f2d78286e730236b1b993728ca1ce3a27cd801
Tzeentche <ilyasugako87@gmail.com>
2020-01-16 22:51:16 +0300

    Added methods to decision for task 3.2.7. (EPAM, 3.String and basics of text processing)

--- src/edu/epam/StringBuilder/task7/SpaceKiller.java
+++ src/edu/epam/StringBuilder/task7/SpaceKiller.java
@@ -3,10 +3,47 @@ package edu.epam.StringBuilder.task7;
 //      Вводится строка. Требуется удалить из нее повторяющиеся символы и все пробелы. Например,
 //      если было введено "abc cde def", то должно быть выведено "abcdef".
 
+import java.util.*;
+
 public class SpaceKiller {
 
     public static void main(String[] args) {
 
-        
+
+        SpaceKiller spaceKiller = new SpaceKiller();
+        spaceKiller.newSentence();
+    }
+
+    private void newSentence() {
+
+        Scanner scn = new Scanner(System.in);
+        String sentense = "";
+
+        System.out.println("Please, enter your sentense: ");
+
+        try {
+
+            sentense = scn.nextLine();
+            scn.close();
+
+        } catch(InputMismatchException ex1) {
+
+            System.out.println("You done something wrong, and broke it all.");
+        }
+
+        cloneKiller(sentense);
+    }
+
+    private void cloneKiller(String usersExpression) {
+
+
+    }
+
+    private void spaceKiller() {
+
+    }
+
+    private void outputResult() {
+
     }
 }

24b55d37e7c79c76d2c3d13a1f9ec8436fa88b80
Tzeentche <ilyasugako87@gmail.com>
2020-01-17 23:30:52 +0300

    Add logical part to task 3.2.10 (EPAM, 3.String and basics of text processing)

--- src/edu/epam/StringBuilder/task10/StringX.java
+++ src/edu/epam/StringBuilder/task10/StringX.java
@@ -3,9 +3,23 @@ package edu.epam.StringBuilder.task10;
 //      Строка X состоит из нескольких предложений, каждое из которых кончается точкой, восклицательным
 //      или вопросительным знаком. Определить количество предложений в строке X.
 
+import java.util.*;
+
 public class StringX {
 
     public static void main(String[] args) {
 
+    }
+
+    private void someText() {
+
+    }
+
+    private void dotsCounter() {
+
+    }
+
+    private void resultOutput() {
+        
     }
 }
--- src/edu/epam/StringBuilder/task9/UpperLowerCase.java
+++ src/edu/epam/StringBuilder/task9/UpperLowerCase.java
@@ -26,6 +26,6 @@ public class UpperLowerCase {
     }
 
     private void result() {
-        
+
     }
 }

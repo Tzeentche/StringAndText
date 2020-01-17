f26e7ae0f0b6a6a1fc59692f0229937ac2c3501c
Tzeentche <ilyasugako87@gmail.com>
2020-01-17 23:29:19 +0300

    Add logical part to task 3.2.9 (EPAM, 3.String and basics of text processing)

--- src/edu/epam/StringBuilder/task9/UpperLowerCase.java
+++ src/edu/epam/StringBuilder/task9/UpperLowerCase.java
@@ -3,10 +3,29 @@ package edu.epam.StringBuilder.task9;
 //      Посчитать количество строчных (маленьких) и прописных (больших) букв в введенной строке.
 //      Учитывать только английские буквы.
 
+import java.util.*;
+
 public class UpperLowerCase {
 
     public static void main(String[] args) {
 
+        UpperLowerCase upperLowerCase = new UpperLowerCase();
+        upperLowerCase.usersLine();
+    }
 
+    private void usersLine() {
+
+    }
+
+    private void lowerCounter() {
+
+    }
+
+    private void upperCounter() {
+
+    }
+
+    private void result() {
+        
     }
 }

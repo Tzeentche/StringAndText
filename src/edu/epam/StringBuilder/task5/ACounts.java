4a057adc1113be49c0a7825bac93ae80d2f94d9e
Tzeentche <ilyasugako87@gmail.com>
2020-01-18 22:16:17 +0300

    Add code part to task 3.2.5 (EPAM, 3.String and basics of text processing)

--- src/edu/epam/StringBuilder/task5/ACounts.java
+++ src/edu/epam/StringBuilder/task5/ACounts.java
@@ -2,29 +2,52 @@ package edu.epam.StringBuilder.task5;
 
 //      Подсчитать, сколько раз среди символов заданной строки встречается буква “а”.
 
+import java.util.*;
+import java.util.regex.*;
+
 public class ACounts {
 
     public static void main(String[] args) {
 
+        ACounts aCounts = new ACounts();
+        aCounts.enteredString();
     }
 
-    String enteredString() {
+    private void enteredString() {
 
-        return "";
+        String userInput = "";
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Please, enter your expression with \"a\" on a board: ");
+
+        try {
+            userInput = scn.nextLine();
+            scn.close();
+
+        } catch(InputMismatchException ex1) {
+
+            System.out.println("Amazing! Everything was crashed!");
+        }
+
+        aCounter(userInput);
     }
 
-    String separator() {
+    private void aCounter(String someWords) {
 
-        return "";
+        int counter = 0;
+        Pattern pattern =Pattern.compile("[a]");
+        Matcher match = pattern.matcher(someWords);
+
+        while (match.find()) {
+
+            counter += 1;
+        }
+
+        output(counter);
     }
 
-    String counter() {
+    private void output(int resultVariable) {
 
-        return "";
-    }
-
-    String output() {
-
-        return "";
+        System.out.println("After long and hard counting we find, that your sentence has " + resultVariable + " chars \"a\"");
     }
 }

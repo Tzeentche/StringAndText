e1c296a0e9a909db63ef0fc948fc2ff658c2314c
Tzeentche <ilyasugako87@gmail.com>
2020-01-20 23:03:49 +0300

    Finished to develop code to task 3.2.8. (EPAM, 3.String and basics of text processing)

--- src/edu/epam/StringBuilder/task8/LongWord.java
+++ src/edu/epam/StringBuilder/task8/LongWord.java
@@ -9,17 +9,56 @@ public class LongWord {
 
     public static void main(String[] args) {
 
+        LongWord longWord = new LongWord();
+        longWord.usersText();
     }
 
     private void usersText() {
 
+        String text = "";
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter your text, please:");
+
+        try {
+
+            text = scn.nextLine();
+            scn.close();
+
+        } catch (InputMismatchException ex1) {
+
+            System.out.println("Something going wrong...");
+        }
+
+        charsSplitter(text);
     }
 
-    private void charsCounter() {
+    private void charsSplitter(String inputText) {
 
+
+        String[] textArray = inputText.split("[ ]");
+
+        charsCounter(textArray);
+    }
+    private void charsCounter(String[] usersTextArray) {
+
+        int textLength = usersTextArray[0].length();
+        String theLongestWord = "";
+
+        for (int counter = 0; counter < usersTextArray.length; counter ++) {
+
+            if (textLength <= usersTextArray[counter].length()) {
+
+                textLength = usersTextArray[counter].length();
+                theLongestWord = usersTextArray[counter];
+            }
+        }
+
+        outputter(theLongestWord);
     }
 
-    private void outputter() {
-        
+    private void outputter(String result) {
+
+        System.out.println("The most long word is " + result + " ! EE!");
     }
 }
